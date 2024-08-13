4a60f21437650ac1e69bbb80063fd698c7a96781
9gods <codingcomelli@gmail.com>
2024-08-12 22:17:52 -0300

    .

--- src/pages/home-page.tsx
+++ src/pages/home-page.tsx
@@ -1,7 +1,10 @@
 namespace HomePage {
   export const Page = () => {
     return (
-      <h1>Hello World</h1>
+      <div>
+        <h1 className="font-hanson text-3xl text-red-400 p-16">Hello World</h1>
+        <h1 className="font-work text-3xl text-red-400 p-16">Hello World</h1>
+      </div>
     )
   }
 }
